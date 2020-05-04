c02574858e746db7422d77ca0715e0a5a0d346e7
ArtemSweb <solodovnikov-artem@inbox.ru>
2020-05-04 13:27:38 +0300

    removed the indents

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-const gulp 					= require('gulp'),
-			browserSync   = require('browser-sync'),
-			cssmin 				= require('gulp-minify-css'),
-			rename  			= require('gulp-rename'); 
+const gulp = require('gulp'),
+	browserSync = require('browser-sync'),
+	cssmin = require('gulp-minify-css'),
+	rename = require('gulp-rename');
 
 gulp.task('style', function () {
 	return gulp.src(['css/*.css', '!css/*.min.css'])
 		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
 		.pipe(gulp.dest('css'));
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
 	browserSync.init({
-			server: {
-					baseDir: "./"
-			}
+		server: {
+			baseDir: "./"
+		}
 	});
 
 	gulp.watch("./*.html").on('change', browserSync.reload);
 	gulp.watch("./css/*.css").on('change', browserSync.reload);
 });
-
-
