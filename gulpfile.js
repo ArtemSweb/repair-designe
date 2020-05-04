2d2162bf48855f5db4d194a2d2036ae819f97e1a
ArtemSweb <solodovnikov-artem@inbox.ru>
2020-05-04 12:59:30 +0300

    wrote a CSS optimization task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
-const gulp = require('gulp'),
-			browserSync   = require('browser-sync');
+const gulp 					= require('gulp'),
+			browserSync   = require('browser-sync'),
+			cssmin 				= require('gulp-minify-css'),
+			rename  			= require( "gulp-rename" ); 
 
-gulp.task('hello', done => {
-	console.log('Привет, мир!');
-	done();
+gulp.task('style', function () {
+	return gulp.src(['css/*.css', '!css/*.min.css'])
+		.pipe(cssmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('css'));
 });
 
-// Static server
 gulp.task('browser-sync', function() {
 	browserSync.init({
 			server: {
 					baseDir: "./"
 			}
 	});
+
 	gulp.watch("./*.html").on('change', browserSync.reload);
+	gulp.watch("./css/*.css").on('change', browserSync.reload);
 });
+
+
