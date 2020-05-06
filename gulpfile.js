00ef7422f46f5660fc5586f1b928e04368ba655a
ArtemSweb <solodovnikov-artem@inbox.ru>
2020-05-06 23:08:38 +0300

    feat: add autoprefixer into gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,21 @@ const {src, dest, watch} = require('gulp'),
 	browserSync = require('browser-sync'),
 	cssmin = require('gulp-minify-css'),
 	rename = require('gulp-rename'),
+	autoprefixer = require('gulp-autoprefixer'),
 	sass = require('gulp-sass');
 
-// function style() {
-// 	return gulp.src(['css/*.css', '!css/*.min.css'])
-// 		.pipe(cssmin())
-// 		.pipe(rename({
-// 			suffix: '.min'
-// 		}))
-// 		.pipe(gulp.dest('css'));
-// };
+function style() {
+	return src(['css/*.css', '!css/*.min.css'])
+		.pipe(cssmin())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(dest('css'));
+};
 
 function bs() {
 	servSass();
+	style();
 	browserSync.init({
 		server: {
 			baseDir: "./"
@@ -29,6 +31,9 @@ function bs() {
 function servSass() {
 	return src("./sass/*.sass")
 		.pipe(sass())
+		.pipe(autoprefixer({
+			cascade: false
+	}))
 		.pipe(dest("./css"))
 		.pipe(browserSync.stream());
 }
