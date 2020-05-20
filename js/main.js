c5bb3ac7aafb6b8a66c4efe0205571621537b53d
ArtemSweb <solodovnikov-artem@inbox.ru>
2020-05-20 19:14:20 +0300

    fix: fixed position alarm message in form

--- js/main.js
+++ js/main.js
@@ -209,7 +209,7 @@ $(document).ready(function () {
 	
 //Маска для телефона
 	$('[type=tel]').mask('+7(000) 000-00-00', {
-		placeholder: "+7 (___) ___-__-__"
+		placeholder: "Ваш номер телефона:"
 	});
 	
 
